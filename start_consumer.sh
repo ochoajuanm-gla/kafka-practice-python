f859af06a6b01534821f9f20f3ce8486504868e1
Juan Ochoa <jochoa@andreani.com>
2022-10-11 13:44:30 -0300

    add shell scripts

---
+++ start_consumer.sh
@@ -0,0 +1,4 @@
+# Correr en otra terminal, previamente habiendo ejecutado start_producer.sh
+cd kafka-practice-python/ &&
+source .venv/bin/activate &&
+python3 kafka_consumer.py
---
+++ start_producer.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:ochoajuanm-gla/kafka-practice-python.git &&
+cd kafka-practice-python/ &&
+python3 -m venv .venv &&
+source .venv/bin/activate &&
+pip install -r requirements.txt &&
+docker-compose up -d &&
+python3 kafka_producer.py
